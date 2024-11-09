b231c0046148539e0436a4cbdce270b45a41784d
Steven357 <stevengonzalez357@gmail.com>
2024-11-08 18:15:56 -0600

    Se crea la clase Inventario y se inicia por crear dentro de ella una Lista llamada Producto.

---
+++ GestionInventario/Inventario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionInventario
+{
+    public class Inventario
+    {
+        private List<Producto> productos;
+
+        public Inventario()
+        {
+            productos = new List<Producto>();
+        }
+
+    }
+}
+   
